4f5ae013c43624987b7bada229c18f84c24ead27
MaxenceRaux <mr.raux@gmail.com>
2024-07-13 00:17:47 +0200

    chore(init): init plugin project

---
+++ project-variable-parser/src/main/java/com/maxenceraux/AnnotationProcessor.java
@@ -0,0 +1,20 @@
+package com.maxenceraux;
+/*
+ * This file is part of the project project-variable-parser.
+ *
+ * project-variable-parser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * project-variable-parser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with My Maven Plugin Project. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+public class AnnotationProcessor {
+}
